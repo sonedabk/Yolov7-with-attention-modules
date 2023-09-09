0ec3c65e527b3cb6ada1f90f9fe475a2ce69a972
tvcuong98 <unikool8989@gmail.com>
2023-09-09 11:09:51 +0700

    mode cbam v3, using mish

--- train_gpus.sh
+++ train_gpus.sh
@@ -1,2 +1,2 @@
 # nproc_per_node = số lượn cuda sử dụn
-python -m torch.distributed.launch --nproc_per_node 2 --master_port 9528 train.py --device 2,3 --sync-bn --workers 8 --batch-size 16 --epochs 500 --data ./visdrone_dataset.yaml --img 640 640 --cfg cfg/training/yolov7-tiny-cbam-v2.yaml --weights '' --hyp data/hyp.scratch.tiny.yaml
+python -m torch.distributed.launch --nproc_per_node 4 --master_port 9528 train.py --device 0,1,2,3 --sync-bn --workers 16 --batch-size 32 --epochs 1000 --data ./visdrone_dataset.yaml --img 640 640 --cfg cfg/training/v7-tiny-cbam-v3.yaml --weights '' --hyp data/hyp.scratch.tiny.yaml
