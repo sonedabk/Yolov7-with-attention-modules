1918d1b183e5a9dc959223a660b561954772c7e9
sonhm <unikool8989@gmail.com>
2023-09-19 17:34:26 +0700

    anchorbox mcp-yolo v1

--- train_v1.sh
+++ train_v1.sh
@@ -1,1 +1,1 @@
-python train.py --workers 8 --device 1 --batch-size 16 --epochs 200 --data ./visdrone_dataset.yaml --img 640 640 --cfg cfg/training/yolov7-tiny-mod-v1.yaml --weights '' --hyp data/hyp.scratch.tiny.yaml
+python train.py --workers 8 --device 0 --batch-size 8 --epochs 500 --data ./visdrone_dataset.yaml --img 640 640 --cfg cfg/training/mcp-yolo.yaml --weights '' --hyp data/hyp.scratch.p5.yaml --name mcp_yolo
